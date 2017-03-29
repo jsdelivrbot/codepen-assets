cc427201a4084d580e0e12953fca31584e98cf93
Tripp Lyons <tripplyons@gmail.com>
2017-03-28 18:00:21 -0700

    Update twitch.js

--- twitch.js
+++ twitch.js
@@ -1,10 +1,11 @@
 simply.title('Hello World!');
-var xhrRequest = function (url, type, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.onload = function () {
-    callback(this.responseText);
-  };
-  xhr.setRequestHeader("Client-ID", "9vd07462fvgwhjxb4767tv020zrriv")
-  xhr.open(type, url);
-  xhr.send();
-};
+ajax({
+ type: 'GET',
+ url: 'https://api.twitch.tv/kraken/channels/twitch',
+ headers: {
+   'Client-ID': '9vd07462fvgwhjxb4767tv020zrriv'
+ },
+ success: function(data) {
+   simply.body(data);
+ }
+});
