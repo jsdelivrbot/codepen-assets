8d461f2bc01bda245cecd93a17bded1cfb490622
Tripp Lyons <tripplyons@gmail.com>
2017-03-28 18:22:32 -0700

    Update twitch.js

--- twitch.js
+++ twitch.js
@@ -1,12 +1,12 @@
 simply.title('Hello World3');
 ajax({
  type: 'GET',
- url: 'https://api.twitch.tv/kraken/channels/twitch',
+ url: 'https://api.twitch.tv/kraken/streams/twitchplayspokemon',
  headers: {
    'Client-ID': '9vd07462fvgwhjxb4767tv020zrriv'
  },
 }, function(data) {
-   simply.body(data);
+   simply.body(data.stream.preview.medium);
 }, function(data) {
-   simply.body(data);
+   simply.body(data.stream.preview.medium);
 });
