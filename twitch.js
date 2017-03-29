bd70f5db4103c0a1473b2f0b00327fdeabe72263
Tripp Lyons <tripplyons@gmail.com>
2017-03-28 17:12:46 -0700

    Update twitch.js

--- twitch.js
+++ twitch.js
@@ -1,1 +1,10 @@
 simply.title('Hello World!');
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.setRequestHeader("Client-ID", "9vd07462fvgwhjxb4767tv020zrriv")
+  xhr.open(type, url);
+  xhr.send();
+};
